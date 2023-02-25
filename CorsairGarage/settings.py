696f622105fc3dd7fc17747bda4dd00bbb26a87d
CorsairRom <ric.romero@duocuc.cl>
2023-02-25 18:41:30 -0300

    prueba5 staticos

--- CorsairGarage/settings.py
+++ CorsairGarage/settings.py
@@ -126,8 +126,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'page/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "page/staticfiles") 
-]
+STATICFILES_DIRS = ("static",)
