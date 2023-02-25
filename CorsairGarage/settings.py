d282a51aaa652cc45304126c4bea202deaa68311
CorsairRom <ric.romero@duocuc.cl>
2023-02-25 19:17:22 -0300

    prueba 11 estaticos

--- CorsairGarage/settings.py
+++ CorsairGarage/settings.py
@@ -128,5 +128,5 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, 'static')
+   os.path.join(BASE_DIR, 'staticfiles')
 ]
