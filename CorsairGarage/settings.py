58a3ac8d80a34007aff9d972fc4a981bb8233b3f
CorsairRom <ric.romero@duocuc.cl>
2023-02-26 18:48:12 -0300

    config

--- CorsairGarage/settings.py
+++ CorsairGarage/settings.py
@@ -12,10 +12,11 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
-
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
