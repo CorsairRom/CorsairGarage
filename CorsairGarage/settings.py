b0f950b657066dcd61b4a91363d7603a12b3ec56
CorsairRom <ric.romero@duocuc.cl>
2023-02-27 03:43:05 -0300

    cambio db

--- CorsairGarage/settings.py
+++ CorsairGarage/settings.py
@@ -81,12 +81,27 @@ WSGI_APPLICATION = 'CorsairGarage.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'railway',
+        'USER': 'root',
+        'PASSWORD': 'AXLqviHblluEhUwKZGJ6',
+        'HOST': 'containers-us-west-123.railway.app',
+        'PORT': '7459',
+        'OPTIONS': {
+            'charset': 'latin1'  # This is the relevant line
+        }
     }
 }
+
 
 
 # Password validation
