5addd45ec3c6f1823cf0958016984ecbcf934d25
CorsairRom <ric.romero@duocuc.cl>
2023-02-28 05:01:20 -0300

    aplicando mapa

--- page/static/js/page/mapscx.js
+++ page/static/js/page/mapscx.js
@@ -1,23 +1,10 @@
-var geocoder;
-var map;
+let map;
 
-function initialize() {
-    geocoder = new google.maps.Geocoder();
-    geocoder.geocode({ 'address': "los muermos 8122, hualpen" }, function (results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-            var mapOptions = {
-                zoom: 15,
-                center: new google.maps.LatLng(-36.788799, -73.090988),
-                mapTypeId: google.maps.MapTypeId.ROADMAP
-            }
-
-            // Let's draw the map
-            map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-
-        }
-        else {
-            alert("Geocode was not successful for the following reason: " + status);
-        }
-    });
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
 }
 
+window.initMap = initMap;
